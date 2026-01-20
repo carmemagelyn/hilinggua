c111c8b73f8ab9a64a00dadd1c07dc81b10e1ab7
carmemagelyn <carmemagelyn@gmail.com>
2026-01-20 09:48:56 +0800

    update

--- src/data/vocabulary.js
+++ src/data/vocabulary.js
@@ -1,43 +1,9 @@
 export const vocabularyData = [
 
 
-  {
-    id: 3,
-    word: "Libro",
-    pronunciation: "li-bro",
-    meaning: "Book",
-    exampleSentence: "Nagbasa siya sang libro.",
-    exampleTranslation: "He/She read a book.",
-    exampleImage: "/asset/img-sentence/He_She read a book..png",
-    image: "/asset/img-word/Libro.png",
-    audio: "/asset/word-voice/Libro.m4a"
-  },
+ 
 
  
 
 
-  {
-    id: 14,
-    word: "Masadya",
-    pronunciation: "ma-sad-ya",
-    meaning: "Happy",
-    exampleSentence: "Daw masadya siya subong.",
-    exampleTranslation: "He/She seems happy now.",
-    exampleImage: "/asset/img-sentence/He_She seems happy now..png",
-    image: "/asset/img-word/Masadya.png",
-    audio: "/asset/word-voice/Masadya.m4a",
-    exampleSentenceAudio: "/asset/word-sent-hil/Daw masadya sya subong.m4a"
-  },
-  {
-    id: 15,
-    word: "Prutas",
-    pronunciation: "pru-tas",
-    meaning: "Fruit",
-    exampleSentence: "Daw matam-is ini nga prutas.",
-    exampleTranslation: "It looks like a sweet fruit.",
-    exampleImage: "/asset/img-sentence/This fruit seems sweet..png",
-    image: "/asset/img-word/Prutas.png",
-    audio: "/asset/word-voice/Prutas.m4a",
-    exampleSentenceAudio: "/asset/word-sent-hil/Daw matam is ini nga prutas.m4a"
-  }
 ];
